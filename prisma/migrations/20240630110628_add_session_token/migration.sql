774d781422799433091fb7bb2f5a5619fc6cb296
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-06-30 23:08:46 +1200

    Update session model to remove default value for accessToken

--- prisma/migrations/20240630110628_add_session_token/migration.sql
+++ prisma/migrations/20240630110628_add_session_token/migration.sql
@@ -1,3 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `accessToken` to the `Session` table without a default value. This is not possible if the table is not empty.
+
+*/
 -- RedefineTables
 PRAGMA defer_foreign_keys=ON;
 PRAGMA foreign_keys=OFF;
@@ -7,7 +13,7 @@ CREATE TABLE "new_Session" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "userId" TEXT NOT NULL,
-    "accessToken" TEXT NOT NULL DEFAULT '',
+    "accessToken" TEXT NOT NULL,
     CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 INSERT INTO "new_Session" ("createdAt", "expirationDate", "id", "updatedAt", "userId") SELECT "createdAt", "expirationDate", "id", "updatedAt", "userId" FROM "Session";
