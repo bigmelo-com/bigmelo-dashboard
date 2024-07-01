83af49802d1a287483868c577f9c409a4f77b132
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-07-01 14:42:25 +1200

    chore: Update requireUserId to requireAuthedSession in download-user-data.tsx

--- app/routes/resources+/download-user-data.tsx
+++ app/routes/resources+/download-user-data.tsx
@@ -1,12 +1,12 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/node'
-import { requireUserId } from '#app/utils/auth.server.ts'
+import { requireAuthedSession } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { getDomainUrl } from '#app/utils/misc.tsx'
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const userId = await requireUserId(request)
+	const sessionData = await requireAuthedSession(request)
 	const user = await prisma.user.findUniqueOrThrow({
-		where: { id: userId },
+		where: { id: sessionData?.userId },
 		// this is one of the *few* instances where you can use "include" because
 		// the goal is to literally get *everything*. Normally you should be
 		// explicit with "select". We're using select for images because we don't
