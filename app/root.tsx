534af773df23b363afd5da6ecf20793c0a4588fe
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-07-03 11:57:21 +1200

    Setup material ui in remix project

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import { withEmotionCache } from '@emotion/react'
+import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/material'
 import {
 	json,
 	type LoaderFunctionArgs,
@@ -6,47 +8,33 @@ import {
 	type MetaFunction,
 } from '@remix-run/node'
 import {
-	Form,
-	Link,
 	Links,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
 	useLoaderData,
-	useMatches,
-	useSubmit,
 } from '@remix-run/react'
 import { withSentry } from '@sentry/remix'
-import { useRef } from 'react'
+import { useContext } from 'react'
 import { HoneypotProvider } from 'remix-utils/honeypot/react'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
-import { EpicProgress } from './components/progress-bar.tsx'
-import { SearchBar } from './components/search-bar.tsx'
 import { useToast } from './components/toaster.tsx'
-import { Button } from './components/ui/button.tsx'
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuPortal,
-	DropdownMenuTrigger,
-} from './components/ui/dropdown-menu.tsx'
-import { Icon, href as iconsHref } from './components/ui/icon.tsx'
-import { EpicToaster } from './components/ui/sonner.tsx'
-import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
+import { href as iconsHref } from './components/ui/icon.tsx'
 import tailwindStyleSheetUrl from './styles/tailwind.css?url'
+import ClientStyleContext from './styles/theme/ClientStyleContext.tsx'
+import Layout from './styles/theme/Layout.tsx'
+import theme from './styles/theme/theme.ts'
 import { getSessionData, logout } from './utils/auth.server.ts'
 import { ClientHintCheck, getHints } from './utils/client-hints.tsx'
 import { prisma } from './utils/db.server.ts'
 import { getEnv } from './utils/env.server.ts'
 import { honeypot } from './utils/honeypot.server.ts'
-import { combineHeaders, getDomainUrl, getUserImgSrc } from './utils/misc.tsx'
+import { combineHeaders, getDomainUrl } from './utils/misc.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
-import { type Theme, getTheme } from './utils/theme.server.ts'
+import { getTheme } from './utils/theme.server.ts'
 import { makeTimings, time } from './utils/timing.server.ts'
 import { getToast } from './utils/toast.server.ts'
-import { useOptionalUser, useUser } from './utils/user.ts'
 
 export const links: LinksFunction = () => {
 	return [
@@ -149,109 +137,89 @@ export const headers: HeadersFunction = ({ loaderHeaders }) => {
 	return headers
 }
 
-function Document({
-	children,
-	nonce,
-	theme = 'light',
-	env = {},
-	allowIndexing = true,
-}: {
+type DocumentProps = {
 	children: React.ReactNode
 	nonce: string
-	theme?: Theme
 	env?: Record<string, string>
 	allowIndexing?: boolean
-}) {
-	return (
-		<html lang="en" className={`${theme} h-full overflow-x-hidden`}>
-			<head>
-				<ClientHintCheck nonce={nonce} />
-				<Meta />
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				{allowIndexing ? null : (
-					<meta name="robots" content="noindex, nofollow" />
-				)}
-				<Links />
-			</head>
-			<body className="bg-background text-foreground">
-				{children}
-				<script
-					nonce={nonce}
-					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(env)}`,
-					}}
-				/>
-				<ScrollRestoration nonce={nonce} />
-				<Scripts nonce={nonce} />
-			</body>
-		</html>
-	)
 }
+
+const Document = withEmotionCache(
+	(
+		{ children, nonce, env = {}, allowIndexing = true }: DocumentProps,
+		emotionCache,
+	) => {
+		const clientStyleData = useContext(ClientStyleContext)
+
+		// Only executed on client
+		useEnhancedEffect(() => {
+			// re-link sheet container
+			emotionCache.sheet.container = document.head
+			// re-inject tags
+			const tags = emotionCache.sheet.tags
+			emotionCache.sheet.flush()
+			tags.forEach(tag => {
+				;(emotionCache.sheet as any)._insertTag(tag)
+			})
+			// reset cache to reapply global styles
+			clientStyleData.reset()
+		}, [])
+
+		return (
+			<html lang="en" className={`${theme} h-full overflow-x-hidden`}>
+				<head>
+					<ClientHintCheck nonce={nonce} />
+					<Meta />
+					<meta charSet="utf-8" />
+					<meta name="viewport" content="width=device-width,initial-scale=1" />
+					<meta name="theme-color" content={theme.palette.primary.main} />
+					{allowIndexing ? null : (
+						<meta name="robots" content="noindex, nofollow" />
+					)}
+					<Links />
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin=""
+					/>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+					/>
+					<meta
+						name="emotion-insertion-point"
+						content="emotion-insertion-point"
+					/>
+				</head>
+				<body className="bg-background text-foreground">
+					{children}
+					<script
+						nonce={nonce}
+						dangerouslySetInnerHTML={{
+							__html: `window.ENV = ${JSON.stringify(env)}`,
+						}}
+					/>
+					<ScrollRestoration nonce={nonce} />
+					<Scripts nonce={nonce} />
+				</body>
+			</html>
+		)
+	},
+)
 
 function App() {
 	const data = useLoaderData<typeof loader>()
 	const nonce = useNonce()
-	const user = useOptionalUser()
-	const theme = useTheme()
-	const matches = useMatches()
-	const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
-	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 	const allowIndexing = data.ENV.ALLOW_INDEXING !== 'false'
 	useToast(data.toast)
 
 	return (
-		<Document
-			nonce={nonce}
-			theme={theme}
-			allowIndexing={allowIndexing}
-			env={data.ENV}
-		>
-			<div className="flex h-screen flex-col justify-between">
-				<header className="container py-6">
-					<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-						<Logo />
-						<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-							{searchBar}
-						</div>
-						<div className="flex items-center gap-10">
-							{user ? (
-								<UserDropdown />
-							) : (
-								<Button asChild variant="default" size="lg">
-									<Link to="/login">Log In</Link>
-								</Button>
-							)}
-						</div>
-						<div className="block w-full sm:hidden">{searchBar}</div>
-					</nav>
-				</header>
-
-				<div className="flex-1">
-					<Outlet />
-				</div>
-
-				<div className="container flex justify-between pb-5">
-					<Logo />
-					<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
-				</div>
-			</div>
-			<EpicToaster closeButton position="top-center" theme={theme} />
-			<EpicProgress />
+		<Document nonce={nonce} allowIndexing={allowIndexing} env={data.ENV}>
+			<Layout>
+				<Outlet />
+			</Layout>
 		</Document>
-	)
-}
-
-function Logo() {
-	return (
-		<Link to="/" className="group grid leading-snug">
-			<span className="font-light transition group-hover:-translate-x-1">
-				Bigmelo
-			</span>
-			<span className="font-bold transition group-hover:translate-x-1">
-				Dashboard
-			</span>
-		</Link>
 	)
 }
 
@@ -265,67 +233,6 @@ function AppWithProviders() {
 }
 
 export default withSentry(AppWithProviders)
-
-function UserDropdown() {
-	const user = useUser()
-	const submit = useSubmit()
-	const formRef = useRef<HTMLFormElement>(null)
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button asChild variant="secondary">
-					<Link
-						to={`/users/${user.username}`}
-						// this is for progressive enhancement
-						onClick={e => e.preventDefault()}
-						className="flex items-center gap-2"
-					>
-						<img
-							className="h-8 w-8 rounded-full object-cover"
-							alt={user.name ?? user.username}
-							src={getUserImgSrc(user.image?.id)}
-						/>
-						<span className="text-body-sm font-bold">
-							{user.name ?? user.username}
-						</span>
-					</Link>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuPortal>
-				<DropdownMenuContent sideOffset={8} align="start">
-					<DropdownMenuItem asChild>
-						<Link prefetch="intent" to={`/users/${user.username}`}>
-							<Icon className="text-body-md" name="avatar">
-								Profile
-							</Icon>
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem asChild>
-						<Link prefetch="intent" to={`/users/${user.username}/notes`}>
-							<Icon className="text-body-md" name="pencil-2">
-								Notes
-							</Icon>
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem
-						asChild
-						// this prevents the menu from closing before the form submission is completed
-						onSelect={event => {
-							event.preventDefault()
-							submit(formRef.current)
-						}}
-					>
-						<Form action="/logout" method="POST" ref={formRef}>
-							<Icon className="text-body-md" name="exit">
-								<button type="submit">Logout</button>
-							</Icon>
-						</Form>
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenuPortal>
-		</DropdownMenu>
-	)
-}
 
 export function ErrorBoundary() {
 	// the nonce doesn't rely on the loader so we can access that
