0988ee1db307224c9d88915c291adbd2eac68a96
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-07-08 08:47:26 +1200

    feat: Add marketing route and component

---
+++ app/routes/_marketing+/index.tsx
@@ -0,0 +1,19 @@
+import { Box, Stack } from '@mui/material'
+import { type MetaFunction } from '@remix-run/node'
+
+export const meta: MetaFunction = () => [{ title: 'Bigmelo' }]
+
+export default function Index() {
+	return (
+		<Box
+			sx={{
+				maxWidth: 'var(--Content-maxWidth)',
+				m: 'var(--Content-margin)',
+				p: 'var(--Content-padding)',
+				width: 'var(--Content-width)',
+			}}
+		>
+			<Stack spacing={4}>Add Marketing Content Here</Stack>
+		</Box>
+	)
+}
