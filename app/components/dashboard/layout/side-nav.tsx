8e7ec24a67cb6aaa0ed22801f9006de77110ef42
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-07-27 10:24:37 +1200

    Fix tests

--- app/components/dashboard/layout/side-nav.tsx
+++ app/components/dashboard/layout/side-nav.tsx
@@ -64,7 +64,7 @@ export function SideNav({
 						href={paths.home}
 						sx={{ display: 'inline-flex' }}
 					>
-						<Logo color={logoColor} height={32} width={122} />
+						<Logo height={32} width={122} />
 					</Box>
 				</div>
 				{/* <WorkspacesSwitch /> */}
