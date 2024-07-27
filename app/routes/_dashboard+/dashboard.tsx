de539f49d83d1e8125402628fbd3ca164216bdd8
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-07-28 09:38:02 +1200

    refactor: Update dashboard component to include loader function and API call for organisations

--- app/routes/_dashboard+/dashboard.tsx
+++ app/routes/_dashboard+/dashboard.tsx
@@ -1,14 +1,41 @@
 import { Box, GlobalStyles } from '@mui/material'
-import { type MetaFunction } from '@remix-run/node'
-import { Outlet } from '@remix-run/react'
+import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
+import { json, Outlet, useLoaderData } from '@remix-run/react'
 import { Fragment } from 'react'
-import { layoutConfig } from '#app/components/dashboard/layout/config.js'
-import { MainNav } from '#app/components/dashboard/layout/main-nav.js'
-import { SideNav } from '#app/components/dashboard/layout/side-nav.js'
-import { EpicProgress } from '#app/components/progress-bar.js'
-import { EpicToaster } from '#app/components/ui/sonner.js'
+import { layoutConfig } from '@/components/dashboard/layout/config.js'
+import { MainNav } from '@/components/dashboard/layout/main-nav.js'
+import { SideNav } from '@/components/dashboard/layout/side-nav.js'
+import { EpicProgress } from '@/components/progress-bar.js'
+import { EpicToaster } from '@/components/ui/sonner.js'
+import { organisationsApiResponseSchema } from '@/types/bigmelo/organisations.js'
+import { get } from '@/utils/api.js'
+import { requireAuthedSession } from '@/utils/auth.server.js'
+import handleLoaderError from '@/utils/server/handleLoaderError.js'
+import { verifyZodSchema } from '@/utils/verifyZodSchema.js'
 
 export const meta: MetaFunction = () => [{ title: 'Dashboard' }]
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const { authHeader } = await requireAuthedSession(request)
+	try {
+		const organisationsResponse = await get('/v1/organization', {
+			headers: {
+				...authHeader,
+			},
+		})
+
+		const verifiedOrganisations = verifyZodSchema(
+			organisationsResponse.data,
+			organisationsApiResponseSchema,
+		)
+
+		return json({
+			organisations: verifiedOrganisations.data,
+		})
+	} catch (error) {
+		return handleLoaderError(error)
+	}
+}
 
 export default function Index() {
 	return (
