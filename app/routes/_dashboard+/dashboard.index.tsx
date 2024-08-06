4f9ca83e7bcc9d077f2132eeba4b6d6db5291b95
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2024-08-06 12:22:32 +1200

    refactor: Update dashboard to redirect to the first organisation

--- app/routes/_dashboard+/dashboard.index.tsx
+++ app/routes/_dashboard+/dashboard.index.tsx
@@ -1,35 +1,19 @@
-import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
-import { redirect } from '@remix-run/react'
-import { organisationsApiResponseSchema } from '#app/types/bigmelo/organisations.js'
-import { get } from '#app/utils/api.js'
-import { requireAuthedSession } from '#app/utils/auth.server.js'
-import handleLoaderError from '#app/utils/server/handleLoaderError.js'
-import { verifyZodSchema } from '#app/utils/verifyZodSchema.js'
+import {
+	redirect,
+	type LoaderFunctionArgs,
+	type MetaFunction,
+} from '@remix-run/node'
+import { getCurrentOrganisationId } from '#app/utils/organisations.server.js'
 
 export const meta: MetaFunction = () => [{ title: 'Bigmelo' }]
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const { authHeader } = await requireAuthedSession(request)
+	const currentOrganisationId = getCurrentOrganisationId(request)
 
-	try {
-		const organisationsResponse = await get('/v1/organization', {
-			headers: {
-				...authHeader,
-			},
-		})
-
-		const verifiedOrganisations = verifyZodSchema(
-			organisationsResponse.data,
-			organisationsApiResponseSchema,
-		)
-
-		if (verifiedOrganisations.data.length > 0) {
-			return redirect(`/dashboard/${verifiedOrganisations.data[0]?.id}`)
-		} else {
-			// TODO - Redirect to create organisation page
-			return redirect(`/logout`)
-		}
-	} catch (error) {
-		return handleLoaderError(error)
+	if (currentOrganisationId) {
+		return redirect(`/dashboard/${currentOrganisationId}`)
+	} else {
+		// TODO - Redirect to create organisation page
+		return redirect('/logout')
 	}
 }
